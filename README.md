c173514cff8fa396c2cb5adab39e2e0e132f8eea
m.lindenberg <m.lindenberg@adcell.de>
2016-02-01 21:15:04 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+Serverless Plugin SNS
+=====================
+
+This plugin easily subscribes your lambda functions to SNS notifications.
+
+*Note*: This plugin supports Serverless 0.2.* 
+
+
+### Installation
+
+ - make sure that aws and serverless are installed
+ - @see http://docs.aws.amazon.com/cli/latest/userguide/installing.html
+ - @see http://www.serverless.com/
+
+ - install this plugin to your projects node_modules folder
+
+```
+cd projectfolder/node_modules/
+git clone https://github.com/martinlindenberg/serverless-plugin-sns.git serverless-plugin-sns
+```
+
+ - install projects dependencies
+
+```
+cd projectfolder/node_modules/
+npm install serverless-plugin-sns --prefix=. --save
+```
+
+ - add the plugin to your s-project.json file
+
+```
+"plugins": [
+    "serverless-plugin-sns"
+]
+
+### Run the Plugin
+
+ - the plugin uses a hook that is called after each deployment of a function 
+ - you only have to deploy your function as usual `sls function deploy`
+ - modify the s-function.json in your functions folder to bind the function to a sns notification
+
+```
+  ...
+  "sns": {
+    "topic": "your-dev-sns-topic"
+  },
+  ...
+```
+
+ - the topic will be created automatically, if not yet done
+
+### Next Steps
+
+ - create notifications that push events to sns topics
+ - for example: cloudwatch alerts can submit notifications to sns topics 
+ - @see https://github.com/martinlindenberg/serverless-plugin-alerting :)
---
+++ index.js
@@ -0,0 +1,237 @@
+'use strict';
+
+module.exports = function(SPlugin) {
+
+    const AWS      = require('aws-sdk'),
+        path       = require('path'),
+        fs         = require('fs'),
+        BbPromise  = require('bluebird'); // Serverless uses Bluebird Promises and we recommend you do to because they provide more than your average Promise :)
+
+    class ServerlessPluginSNS extends SPlugin {
+        constructor(S) {
+            super(S);
+        }
+
+        static getName() {
+            return 'com.serverless.' + ServerlessPluginSNS.name;
+        }
+
+        registerHooks() {
+
+            this.S.addHook(this._addSNSAfterDeploy.bind(this), {
+                action: 'functionDeploy',
+                event:  'post'
+            });
+            this.S.addHook(this._addSNSAfterDeploy.bind(this), {
+                action: 'dashDeploy',
+                event:  'post'
+            });
+
+            return BbPromise.resolve();
+        }
+
+        /**
+         * adds alerts after the deployment of a function
+         *
+         * @param object evt
+         *
+         * @return promise
+         */
+        _addSNSAfterDeploy(evt) {
+            let _this = this;
+
+            return new BbPromise(function(resolve, reject) {
+                for(var region in evt.data.deployed) {
+                    _this._manageSNS(evt, region);
+                }
+
+                return resolve(evt);
+            });
+        }
+
+        /**
+         * Handles the Creation of an alert and the required topics
+         *
+         * @param object evt Event
+         * @param string region
+         *
+         * @return promise
+         */
+        _manageSNS (evt, region) {
+            let _this = this;
+
+            _this.stage = evt.options.stage;
+            _this._initAws(region);
+
+            if (_this.S.cli.action != 'deploy' || (_this.S.cli.context != 'function' && _this.S.cli.context != 'dash'))
+                return;
+
+            _this.functionSNSSettings = _this._getFunctionsSNSSettings(evt, region);
+
+            // no alert.json found
+            if (_this.functionSNSSettings.length == 0) {
+                return;
+            }
+
+            _this._manageTopics(_this.functionSNSSettings)
+            .then(function(){
+                let _this = this;
+                _this._bindFunctions(_this.functionSNSSettings);
+            }.bind(_this))
+        }
+
+        _bindFunctions (settings) {
+            let _this = this;
+
+            for (var i in settings) {
+                var functionArn = settings[i].deployed.Arn;
+                functionArn = functionArn.split(':');
+                functionArn.pop();
+                functionArn = functionArn.join(':');
+                var sns = settings[i].sns.topic;
+
+                console.log('binding function ' + settings[i].deployed.function + ' to topic ' + sns);
+                _this.sns.subscribeAsync({
+                    'Protocol': 'lambda',
+                    'TopicArn': _this._getTopicArnByFunctionArn(functionArn, sns),
+                    'Endpoint': functionArn,
+                })
+                .then(function(result){
+                    console.log('done');
+                    // console.log('result', result);
+                });
+            }
+        }
+
+        _getTopicArnByFunctionArn(functionArn, topicName){
+            var start = functionArn.split(':function:');
+            var topicArn = start[0] + ':' + topicName;
+
+            topicArn = topicArn.replace(':lambda:', ':sns:');
+            return topicArn;
+        }
+
+        /**
+         * collects the topics and calls create topcs
+         *
+         * @param array settings
+         *
+         * @return BpPromise
+         */
+        _manageTopics(settings) {
+            var _this = this;
+
+            var topics = [];
+            for (var i in settings) {
+                var topic = settings[i].sns.topic;
+                topics[topic] = topic;
+            }
+
+            return _this._createTopics(topics);
+        }
+
+        /**
+         * creates topics if not yet done
+         *
+         * @param array topics
+         *
+         * @return BpPromise
+         */
+        _createTopics (topics) {
+            var _this = this;
+            _this.topics = topics;
+
+            return _this.sns.listTopicsAsync()
+            .then(function(topicListResult){
+                var _this = this;
+                //create fast checkable topiclist['topic1'] = 'topic1'
+                var topicList = [];
+                if (topicListResult['Topics']) {
+                    for (var i in topicListResult.Topics) {
+                        var arnParts = topicListResult.Topics[i].TopicArn.split(':')
+                        var topicName = arnParts[arnParts.length - 1];
+                        topicList[topicName] = topicName;
+                    }
+                }
+                
+                for (var i in this.topics) {
+                    if (!topicList[i]) {
+                        console.log('topic ' + i + ' does not exist. it will be created now');
+                        _this.sns.createTopicAsync({
+                            'Name': i
+                        })
+                        .then(function(){
+                            console.log('topic created');
+                        })
+                        .catch(function(e){
+                            console.log('error during creation of the topic !', e)
+                        });
+                    } else {
+                        console.log('topic ' + i + ' exists.');
+                    }
+                }                
+            }.bind(this));
+        }
+
+        /**
+         * initializes aws
+         *
+         * @param string region
+         *
+         * @return void
+         */
+        _initAws (region) {
+            let _this = this;
+
+            _this.sns = new AWS.SNS({
+                region: region,
+                accessKeyId: this.S.config.awsAdminKeyId,
+                secretAccessKey: this.S.config.awsAdminSecretKey
+            });
+
+            BbPromise.promisifyAll(_this.sns);
+        }
+
+
+        /**
+         * parses the sns-function.json file and returns the data
+         *
+         * @param object evt
+         * @param string region
+         *
+         * @return array
+         */
+        _getFunctionsSNSSettings(evt, region){
+            let _this = this;
+            var settings = [];
+            for (var deployedIndex in evt.data.deployed[region]) {
+                let deployed = evt.data.deployed[region][deployedIndex],
+                    settingsFile = _this.S.config.projectPath + '/' + deployed.component + '/' + deployed.module + '/' + deployed.function + '/s-function.json';
+
+                if (!fs.existsSync(settingsFile)) {
+                    continue;
+                }
+
+                try {
+                    var config = JSON.parse(fs.readFileSync(settingsFile));
+
+                    if (!config.sns) {
+                        continue;
+                    }
+
+                    settings.push({
+                        "deployed": deployed,
+                        "sns": config.sns
+                    });
+                } catch (e) {
+                    console.log('alerting.json not readable');
+                    continue;
+                }            
+            }
+
+            return settings;
+        }
+    }
+
+    return ServerlessPluginSNS;
+};
